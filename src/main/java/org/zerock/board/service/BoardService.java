57bb7def5da4c51eeddfce2af95599a76c3ebe92
kwangho kwak <678hoa@gmail.com>
2021-07-29 14:23:34 +0900

    BoardService 인터페이스 내용 수정.

--- src/main/java/org/zerock/board/service/BoardService.java
+++ src/main/java/org/zerock/board/service/BoardService.java
@@ -7,10 +7,20 @@ import org.zerock.board.entity.Member;
 public interface BoardService {
     Long register(BoardDTO dto);
 
-    default Board dtoToEntity(BoardDTO dto) {
+    default Board dtoToEntity(BoardDTO dto, Member member, Long replyCount) {
         Member member = Member.builder().email(dto.getWriterEmail()).build();
 
-        Board board = Board.builder().bno(dto.getBno()).title(dto.getTitle()).content(dto.getContent()).writer(member).build();
-        return board;
+        BoardDTO boardDTO = BoardDTO.builder()
+        .bno(board.getBno())
+        .title(board.getTitle())
+        .content(board.getContent())
+        .regDate(board.getRegDate())
+        .modDate(board.getModDate())
+        .writerEmail(member.getEmail())
+        .writerName(member.getName())
+        .replyCount(replyCount.intValue())
+        .build()
+
+        return boardDTO;
     }
 }
